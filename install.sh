4c34778e4deb1f92f712ed8c03803967b14c7231
Yueqing Zhang <yuz75@pitt.edu>
2023-07-14 22:04:01 +0800

    bug fix

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "$password" | sudo -S apt upgrade
 
 echo "$password" | sudo -S apt install git
 cat config.txt | grep email | awk ' { print $2 } ' | xargs git config --global user.email
-cat config.txt | grep name | awk '{ for(i=2; i<=NF; i++) printf "%s ", $i; }' | | rev | cut -c 2- | rev | sed 's/.*/"&"/' | xargs git config --global user.name
+cat config.txt | grep name | awk '{ for(i=2; i<=NF; i++) printf "%s ", $i; }' | rev | cut -c 2- | rev | sed 's/.*/"&"/' | xargs git config --global user.name
 echo "$password" | sudo -S apt install cmake
 echo "$password" | sudo -S apt install build-essential
 echo "$password" | sudo -S apt install pip
