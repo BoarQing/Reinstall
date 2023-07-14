12d5557a10077126de584e35952f9574746e0337
Yueqing Zhang <yuz75@pitt.edu>
2023-07-14 22:02:28 +0800

    init

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if password is provided as argument
+if [ -z "$1" ]; then
+  echo "Error: Please provide the password as the first argument."
+  exit 1
+fi
+
+password="$1"
+
+echo "$password" | sudo -S apt update
+echo "$password" | sudo -S apt upgrade
+
+echo "$password" | sudo -S apt install git
+cat config.txt | grep email | awk ' { print $2 } ' | xargs git config --global user.email
+cat config.txt | grep name | awk '{ for(i=2; i<=NF; i++) printf "%s ", $i; }' | | rev | cut -c 2- | rev | sed 's/.*/"&"/' | xargs git config --global user.name
+echo "$password" | sudo -S apt install cmake
+echo "$password" | sudo -S apt install build-essential
+echo "$password" | sudo -S apt install pip
+echo "$password" | sudo -S apt install vim
+
+pip install -r requirement.txt
